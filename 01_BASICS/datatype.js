82ac33387bc647fec28b9e2f37c7692818d9b6c0
Anuraj1709 <109717926+Anuraj1709@users.noreply.github.com>
2024-03-07 20:07:03 +0000

    conversion

---
+++ 01_BASICS/datatype.js
@@ -0,0 +1,19 @@
+let score=null
+
+// console.log(typeof score);
+// console.log(score);
+
+let valueInNumber=Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+let logged=""
+
+let boolconverted=Boolean(logged)
+console.log(boolconverted);
+
+let strang=42
+
+let converted=String(strang)
+console.log(converted);
+console.log(typeof converted);
---
+++ 01_BASICS/variable.js
@@ -0,0 +1,8 @@
+const accountId=144453
+let accountmail="raj@google.com"
+var accountPassword="bangalore"
+let accountState;
+// accountId=2
+
+console.log(accountId);
+console.log([accountmail,accountPassword,accountState]);
